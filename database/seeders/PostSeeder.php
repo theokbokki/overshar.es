32be048ac0146a652a55a77643e61603778737d8
théoo <90846584+theokbokki@users.noreply.github.com>
2026-05-16 14:39:36 +0200

    Add about page to seeder

--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -12,6 +12,17 @@ class PostSeeder extends Seeder
      */
     public function run(): void
     {
-        Post::factory(20)->create();
+        Post::factory()->create([
+            'content' => <<<HEREDOC
+            Welcome to overshar.es. This is the place where I say whatever comes through my brain wether it makes sense or not. It’s weird, it’s raw and can be cringe at times. It’s a window to my mind that I’m inviting you to look through.
+
+            If you have thoughts, opinions or simply want to say hi, do it at [theo@overshar.es](mailto:theo@overshar.es)
+            HEREDOC,
+            'color' => '0, 0%, 0%',
+        ]);
+
+        if(env('APP_ENV') === 'local') {
+            Post::factory(20)->create();
+        }
     }
 }
